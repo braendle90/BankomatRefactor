74f298700697350e4f1155610449a69fe3b9e87d
dominik <d.braendle@aub.at>
2021-03-17 08:04:09 +0100

    Initial commit

---
+++ src/com/company/Bankomat_refactor.java
@@ -0,0 +1,68 @@
+import com.company.classes.BankomatRefactorClass;
+
+
+
+
+public class Bankomat_refactor {
+
+
+    public static void main(String[] args) {
+
+        String test = "";
+        int valueOfScannerInput = 0;
+
+
+        BankomatRefactorClass.printMenu();
+
+
+        while (valueOfScannerInput != 4) {
+
+
+            valueOfScannerInput = BankomatRefactorClass.inputScanner.nextInt();
+
+
+            switch (valueOfScannerInput) {
+
+                case 1:
+                    BankomatRefactorClass.DepositToAccoutn();
+
+
+                    break;
+
+                case 2:
+
+                    BankomatRefactorClass.withdrawMoney();
+
+                    break;
+
+                case 3:
+                    outputbankBalance();
+
+                    break;
+                case 4:
+                    System.out.println("Rechner beenden!");
+                    break;
+                default:
+                    System.out.println("Bitte geben Sie eine gültige Zahl zwischen 1 - 4 ein.");
+                    break;
+
+            }
+
+        }
+
+
+    }
+
+
+    public static void outputbankBalance() {
+
+        System.out.println("Ihr Kontostand ist: " + (int) BankomatRefactorClass.bankBalance);
+
+
+    }
+
+
+}
+
+
+
